a5917d1ee837091fd26331353503fb4977c7acb9
Erik Onarheim <erik.onarheim@gmail.com>
2024-02-16 09:39:52 -0600

    update to latest

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,15 @@
-import { Engine, Loader } from "excalibur";
+import { Engine } from "excalibur";
 import { Player } from "./player";
-import { Resources } from "./resources";
+import { loader } from "./resources";
 
 class Game extends Engine {
     constructor() {
       super({width: 800, height: 600});
     }
     initialize() {
-      
       const player = new Player();
       this.add(player);
 
-      const loader = new Loader([Resources.Sword]);
       this.start(loader);
     }
   }
--- src/resources.ts
+++ src/resources.ts
@@ -1,8 +1,11 @@
-import { ImageSource } from "excalibur";
+import { ImageSource, Loader } from "excalibur";
 import sword from "./images/sword.png";
 
-let Resources = {
+export const Resources = {
   Sword: new ImageSource(sword)
-};
+} as const;
 
-export { Resources };
+export const loader = new Loader();
+for (const res of Object.values(Resources)) {
+  loader.addResource(res);
+}
